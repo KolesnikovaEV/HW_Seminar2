b26044a263ada3f15b61f0eb39653f966eb65e7d
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-07-19 15:13:31 +0300

    Решили задачу №10

---
+++ HW001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+int number = new Random(). Next(100, 1000);
+Console.WriteLine($"number:{number}");
+
+int FindNumber = number/10%10;
+Console.WriteLine($"FindNumber:{FindNumber}");
+
