2e2f255fe43e0cd8dfd0c882e1f40c11d4c909db
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-07-19 15:37:10 +0300

    Решили задачу №13

---
+++ HW002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+int number = new Random(). Next(1,10000);
+Console.WriteLine($"number: {number}");
+if(number<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while(number>999)
+    {
+        number = number/10;
+    }
+int FindNumber = number%10;
+Console.WriteLine($"Третья цифра: {FindNumber}");
+}
