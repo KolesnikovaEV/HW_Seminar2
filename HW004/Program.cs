d9f62d3dd8daa89a378bbd2e1066443e3ae97a69
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-07-19 16:43:26 +0300

    Решили задачу со звездочкой

---
+++ HW004/Program.cs
@@ -0,0 +1,13 @@
+﻿// * Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, а на выходе наименования дня
+string[]  daysOfWeek= { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+Console.WriteLine("Введите число: ");
+int number =  Convert.ToInt32(Console.ReadLine());
+if(number < 1 || number > 7)
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
+else
+{
+    Console.WriteLine(daysOfWeek[number-1]);
+}
