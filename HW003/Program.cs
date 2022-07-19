fb517bf70f0f72a316b975dd8c833243628f22dc
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-07-19 16:11:20 +0300

    Решили задачу №15

---
+++ HW003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int number = new Random(). Next(0,10);
+Console.WriteLine(number);
+if(number > 7 || number < 1)
+{
+    Console.WriteLine("Такого дня недели нет");    
+}
+
+if(number == 6 || number == 7)
+{
+   Console.WriteLine("Ура! Выходной!");  
+}
+
+if(number < 6 && number > 0)
+{
+    Console.WriteLine("Будний день. Работай, холоп!"); 
+}
