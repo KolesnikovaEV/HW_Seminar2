5d2d673bc9f2e2e5134339eaa5726e4cccea03b8
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-07-21 14:45:43 +0300

    Решили задачу №23

---
+++ HW005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int N = new Random().Next(1,20);
+Console.WriteLine($"N = {N}");
+int count = 1;
+int stepen = 3;
+while(count <= N)
+{
+    Console.WriteLine($"{count} в кубе = {Math.Pow(count, stepen)}");
+    count++;
+}
+
