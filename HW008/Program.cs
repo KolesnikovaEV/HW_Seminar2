8d80d1fcfd9e9a31ab07d7c467756be8aff4de7a
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-08-04 12:40:02 +0300

    Выполнили задачу №29

---
+++ HW008/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите метод(ы), которай задаёт массив из 8 элементов и выводит их на экран.
+
+int[] Create(int len)
+{
+    return new int[len];
+}
+
+void Fill(int[] arr)
+{
+    int i = 0;
+    while(i < arr.Length)
+    {
+        arr[i] = new Random().Next(0,10);
+        i++;
+    }
+}
+
+void Print(int[] numbers)
+{
+    int index = 0;
+    Console.Write($"[ ");
+    while(index < numbers.Length-1)
+    {
+        Console.Write($"{numbers[index],1}, ");
+        index++;
+    }
+    Console.Write($"{numbers[index]}");
+    Console.Write($" ]");
+    Console.WriteLine();
+}
+
+int[] array = Create(8);
+Fill(array);
+Print(array);
