9c4841a6bb83ee7b01294698f645fcc04d7abfb1
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-08-04 14:15:19 +0300

    Выполнили задачу №25

---
+++ HW009/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine($"Введите число A:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+double Stepen(int a, int b)
+{
+   double c = Math.Pow(a, b);
+   return c;
+}
+
+Console.WriteLine(Stepen(a, b));
