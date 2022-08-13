4076c93411515bae807f8a6005e924ed5ef519aa
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-08-13 16:57:19 +0300

    Решили задачу 36

---
+++ HW012/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] Create(int len)
+{
+    return new int[len];
+}
+
+void Fill(int[] array)
+{
+    for (int i = 0; i<array.Length; i++)
+    {
+        array[i] = new Random(). Next(0,1000);
+    }
+}
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int Summa(int[] numerals)
+{
+    int sum = 0;
+    for (int i = 0; i < numerals.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum+numerals[i];
+        }
+    }
+    return sum;
+}
+
+int[] num = Create(20);
+Fill(num);
+Print(num);
+Console.WriteLine(Summa(num));
