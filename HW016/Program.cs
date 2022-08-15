4b1c13b8265c12947660594530bed056163f2e0f
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-08-15 22:02:39 +0300

    Решили задачу 47

---
+++ HW016/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] Create(int i, int j)
+{
+    return new double[i, j];
+}
+
+void Print(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]:f3} ");
+        }
+       Console.WriteLine(); 
+    }
+    
+}
+
+void Fill(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().NextDouble();
+        }
+    }
+}
+
+double[,] numbers = Create(5, 4);
+Fill(numbers);
+Print(numbers);
