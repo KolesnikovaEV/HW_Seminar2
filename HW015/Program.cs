9c7e17023778c3c598e5d2e000d0135e79704564
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-08-15 21:25:23 +0300

    Доработали задачу 43

--- HW015/Program.cs
+++ HW015/Program.cs
@@ -19,7 +19,9 @@ void Coordinat(double b1, double k1, double b2, double k2)
 {
     double x = (b2 - b1) / (k1 - k2);
     double y = k1 * x + b1;
-    Console.WriteLine($"Координаты точки пересечения: ({x}; {y})");
+    if ((k1 == k2) && (b1 == b2)) Console.WriteLine($"Прямые совпадают");
+    else if (k1==k2) Console.WriteLine($"Прямые параллельны");
+    else Console.WriteLine($"Координаты точки пересечения: ({x}; {y})");
 }
 
 Coordinat(b1, k1, b2, k2);
