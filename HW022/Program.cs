415e30acc2bdc10af1a0824510f828a24ef9c9e3
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-08-24 13:21:54 +0300

    Решили задачу 64

---
+++ HW022/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+
+
+
+void Numbers(int m, int n)
+{
+    
+    while (m < n) 
+    {
+        Console.Write($"{m}, ");
+        m++;
+    }
+    Console.WriteLine($"{n}.");
+    
+}
+
+int m = 4;
+int n = 8;
+
+Numbers(m, n);
