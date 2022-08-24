88b4bd630f6d3eb230e1c551d01ce452a880aef5
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-08-24 13:29:49 +0300

    Выполнили задачу 23

---
+++ HW023/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+void Numbers(int m, int n)
+{
+    int sum = 0;
+    while (m <= n) 
+    {
+        sum = sum + m;
+        m++;
+    }
+    Console.WriteLine($"Сумма: {sum}.");
+    
+}
+
+int m = 1;
+int n = 4;
+
+Numbers(m, n);
